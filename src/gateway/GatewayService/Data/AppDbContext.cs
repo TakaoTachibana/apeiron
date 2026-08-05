e88091cbd081852e587804dcb52a5494daf0f2c5
Takao Tachibna <54224104+TakaoTachibana@users.noreply.github.com>
2026-08-05 19:33:06 +0900

    feat(gateway): connect from julia to mariadb (#9)

--- src/gateway/GatewayService/Data/AppDbContext.cs
+++ src/gateway/GatewayService/Data/AppDbContext.cs
@@ -5,6 +5,20 @@ namespace GatewayService.Data;
 
 public class AppDbContext : DbContext {
 	public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-	public DbSet<Attractor> Attractors => Set<Attractor>();
-}
 
+	public DbSet<Attractor> Attractors => Set<Attractor>();
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder) {
+		base.OnModelCreating(modelBuilder);
+
+		modelBuilder.Entity<Attractor>(entity => {
+			entity.ToTable("attractors");
+
+			entity.Property(e => e.Id).HasColumnName("id");
+			entity.Property(e => e.FormulaLatex).HasColumnName("formula_latex");
+			entity.Property(e => e.RSquared).HasColumnName("r_squared");
+			entity.Property(e => e.IsStable).HasColumnName("is_stable");
+			entity.Property(e => e.CreatedAt).HasColumnName("created_at");
+		});
+	}
+}
